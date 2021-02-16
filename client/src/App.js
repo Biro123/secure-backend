1b2884bb904346d7c6cec13bf5bc63eb4874537d
Biro123 <biro123@ntlworld.com>
2021-02-16 23:14:27 +0000

    Add mainpage, navbar and router

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,25 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
 import 'fontsource-roboto';
+import AppNavBar from './components/appNavBar/AppNavBar';
+import MainPage from './components/mainPage/MainPage';
 
 
 const App = () => {
   return (
     <div className="App">
-      <Typography variant="h4" color="primary" >
-        Johnny's Happy Place        
-      </Typography>
-      <Typography variant="body1" color="textSecondary" gutterBottom>
-        Be excellent to each other!
-      </Typography>
-      <Button variant="contained" color="primary">
-        Hello World
-      </Button>
+      <Router>
+        <AppNavBar />
+        <Switch>
+          <Route exact path='/' component={MainPage} />
+          {/* <Route exact path='/register' component={Register} />
+          <Route exact path='/login' component={Login} /> */}
+        </Switch>
+        {/* <Button variant="contained" color="primary">
+          Hello World
+        </Button> */}
+      </Router>      
     </div>
   );
 }
---
+++ client/src/components/mainPage/MainPage.js
@@ -0,0 +1,15 @@
+import Typography from '@material-ui/core/Typography';
+
+const MainPage = () => {
+  return (
+    <>
+      <Typography variant="h4" color="primary" >
+        Johnny's Happy Place        
+      </Typography>
+      <Typography variant="body1" color="textSecondary" gutterBottom>
+        Be excellent to each other!
+      </Typography>
+    </>
+  )
+}
+export default MainPage;

0c30a83faaff739aad2f003d77c2090f02e11730
Biro123 <biro123@ntlworld.com>
2021-02-16 22:30:23 +0000

    Add material-ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import 'fontsource-roboto';
+
 
 const App = () => {
   return (
     <div className="App">
-      <p>react app</p>
+      <Typography variant="h4" color="primary" >
+        Johnny's Happy Place        
+      </Typography>
+      <Typography variant="body1" color="textSecondary" gutterBottom>
+        Be excellent to each other!
+      </Typography>
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
     </div>
   );
 }

758171ccdfeb71494a28f52aba3dad11bf0674a8
Biro123 <biro123@ntlworld.com>
2021-02-16 20:57:34 +0000

    initial commit for front-end

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ npm run server
 
 #Start in prod mode
 npm run start
+
+#Start both client and server locally
+npm run dev
---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <p>react app</p>
+    </div>
+  );
+}
+
+export default App;

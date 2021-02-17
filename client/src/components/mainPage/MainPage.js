3946a0f7d72293d3b835cb66ad817483b6dfdbc7
Biro123 <biro123@ntlworld.com>
2021-02-17 21:06:48 +0000

    Add simpl read of server data

--- client/src/components/mainPage/MainPage.js
+++ client/src/components/mainPage/MainPage.js
@@ -1,14 +1,35 @@
+import { useState } from '@hookstate/core';
 import Typography from '@material-ui/core/Typography';
+import axios from 'axios';
 
 const MainPage = () => {
+
+  const state = useState(axios.get('/api/users'));
+
+  if (state.promised) {
+    return <p>Loading...</p>
+  }
+
+  if (state.error) {
+    console.error(state.error);
+    return <p>Error...</p>
+  }
+
+  const { data, status } = state.get();
+
+  console.log('status:' + status);
+
   return (
     <>
       <Typography variant="h4" color="primary" >
-        Johnny's Happy Place        
+        Header goes here
       </Typography>
       <Typography variant="body1" color="textSecondary" gutterBottom>
-        Be excellent to each other!
+        And some subtext here
       </Typography>
+      {data.map((entry, index) => 
+        <p key={index}>{entry.name}</p>
+      )}
     </>
   )
 }

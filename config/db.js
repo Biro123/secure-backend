746ea06d28ee1e5221caeb027758744561874ee9
Biro123 <biro123@ntlworld.com>
2021-02-24 23:11:46 +0000

    debug

--- config/db.js
+++ config/db.js
@@ -4,6 +4,7 @@ const db = config.get('mongoURI');
 
 const connectDB = async () => {
   console.log('DEBUG:' + db.substring(1,15));
+  console.log('DEBUG2:' + process.env.mongoURI.substring(1,15));
   try {
     await mongoose.connect(db, {
       useNewUrlParser: true,
